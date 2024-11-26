8b4aea8f3462446ea32097f2437cfc3c486eb769
Angela Barrenechea <angelacba86@gmail.com>
2024-11-26 13:52:07 -0500

    fix<client/src/App>import Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,21 @@
-import { useState } from 'react'
-import {Routes, Route} from "react-router-dom"
-import RegisterForm from './components/register-form/register'
-import { Header } from "./components/header/Header";
-import './App.css'
-import '@fontsource/poppins/200.css';
-import '@fontsource/poppins/400.css';
-import '@fontsource/poppins/600.css';
-import '@fontsource/poppins/800.css';
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import RegisterForm from "./components/register-form/register";
+import {Header} from "./components/header/header";
 
-
-
+import "./App.css";
+import "@fontsource/poppins/200.css";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/800.css";
 
 function App() {
   return (
     <>
+      < Header />
       <Routes>
         <Route exact path="/" element={<RegisterForm />} />
       </Routes>
-      <Header/>
     </>
   );
 }
