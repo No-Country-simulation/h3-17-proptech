0c2596eca395f57433d8a6e31b9857c26592ad02
Angela Barrenechea <angelacba86@gmail.com>
2024-11-30 17:53:32 -0500

    modify <src/App.jsx> add div addContainer,header & footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,7 @@
 import { Header } from "./components/header/header";
 import { Footer } from "./components/footer/footer";
-
-import { useState } from 'react'
-import {Routes, Route} from "react-router-dom"
-import LandingPage from './pages/landing/landing';
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/landing/landing";
 import Register from "./pages/register/registerPage";
 import "./App.css";
 import "@fontsource/poppins/200.css";
@@ -15,12 +13,20 @@ import "./colors.css";
 function App() {
   return (
     <>
-      <Header />
-      <Routes>
-        <Route exact path="/" element={<LandingPage />} />
-        <Route exact path="/register" element={<Register />} />
-      </Routes>
-      <Footer />
+      <div className="appContainer">
+        <header>
+          <Header />
+        </header>
+        <main>
+          <Routes>
+            <Route exact path="/" element={<LandingPage />} />
+            <Route exact path="/register" element={<Register />} />
+          </Routes>
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
     </>
   );
 }
