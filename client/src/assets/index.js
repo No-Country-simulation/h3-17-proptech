00307148103a1bc115e77ee52272e17450f3b266
Angela Barrenechea <angelacba86@gmail.com>
2024-12-02 10:34:26 -0500

    create<src/assets/index.js>import & export assets elements

---
+++ client/src/assets/index.js
@@ -0,0 +1,6 @@
+import buyerImg from "./buyer.png";
+import investorImg from "./investor.png";
+import defaultImg from "./default.jpg";
+import logoFinancial from "./financial.svg";
+
+export { buyerImg, investorImg, defaultImg, logoFinancial };
