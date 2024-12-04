8ef0e2bfc1675050c3c52f48d69a8e1d89fc1c6f
Angela Barrenechea <angelacba86@gmail.com>
2024-12-04 15:46:11 -0500

    create<src/pagez/index.jsx>create index of pages

---
+++ client/src/pages/index.js
@@ -0,0 +1,6 @@
+import { LandingPage } from "./landing/landing";
+import { RegisterPage } from "./register/registerPage";
+import { LoginPage } from "./login/loginPage";
+import { RegisterOptions } from "./register-options/registerOption";
+
+export { LandingPage, RegisterPage, LoginPage, RegisterOptions };
