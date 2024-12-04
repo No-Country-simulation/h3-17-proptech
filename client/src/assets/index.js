a1c957f0f3f6340a171bd767fb9d9c514f1e3ed4
Angela Barrenechea <angelacba86@gmail.com>
2024-12-04 15:29:30 -0500

    create<src/assets/index.js>add bgImg to index

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -2,5 +2,6 @@ import buyerImg from "./buyer.png";
 import investorImg from "./investor.png";
 import defaultImg from "./default.jpg";
 import logoFinancial from "./financial.svg";
+import bgImg from "./bgImg.png";
 
-export { buyerImg, investorImg, defaultImg, logoFinancial };
+export { buyerImg, investorImg, defaultImg, logoFinancial, bgImg };
