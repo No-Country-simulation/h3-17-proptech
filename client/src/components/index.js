5c0f5aa4dfefdfa87944c321e6c38a684e16e17e
Angela Barrenechea <angelacba86@gmail.com>
2024-12-04 15:30:37 -0500

    create<src/components/index.js>import & export components

---
+++ client/src/components/index.js
@@ -0,0 +1,11 @@
+import { Footer as FooterComponent } from "./footer/footer";
+import { Header as HeaderComponent } from "./header/header";
+import { LoginForm as LoginFormComponent } from "./logIn-form/logIn";
+import { RegisterForm as RegisterFormComponent } from "./register-form/register";
+
+export {
+  FooterComponent,
+  HeaderComponent,
+  LoginFormComponent,
+  RegisterFormComponent,
+};
