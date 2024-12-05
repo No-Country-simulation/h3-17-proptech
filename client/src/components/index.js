c6bd8da8e832d578d85af65446456d6ef5d77ee8
Angela Barrenechea <angelacba86@gmail.com>
2024-12-05 15:52:58 -0500

    modify<comonents/index.js>add CardsWhyComponents

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,10 +2,12 @@ import { Footer as FooterComponent } from "./footer/footer";
 import { Header as HeaderComponent } from "./header/header";
 import { LoginForm as LoginFormComponent } from "./logIn-form/logIn";
 import { RegisterForm as RegisterFormComponent } from "./register-form/register";
+import { CardsWhy as CardWhyComponent } from "./cards-why/cardsWhy";
 
 export {
   FooterComponent,
   HeaderComponent,
   LoginFormComponent,
   RegisterFormComponent,
+  CardWhyComponent,
 };
