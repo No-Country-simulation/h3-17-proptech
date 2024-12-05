956da53f4a442f14f719209ec6d0001f0c978210
Angela Barrenechea <angelacba86@gmail.com>
2024-12-05 15:52:21 -0500

    modify<assets/index.js>add images to index

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -3,5 +3,35 @@ import investorImg from "./investor.png";
 import defaultImg from "./default.jpg";
 import logoFinancial from "./financial.svg";
 import bgImg from "./bgImg.png";
+import pic1 from "./1.jpg";
+import pic2 from "./2.jpg";
+import pic3 from "./3.jpg";
+import pic4 from "./4.jpg";
+import pic5 from "./5.jpg";
+import iconCon from "./multiple-icon.png";
+import iconConf from "./personal-icon.png";
+import iconDig from "./newspaper-icon.png";
+import iconSoc from "./receipt-icon.png";
+import howImg1 from "./financiar1.jpg";
+import howImg2 from "./financiar2.jpg";
+import howImg3 from "./financiar3.jpg";
 
-export { buyerImg, investorImg, defaultImg, logoFinancial, bgImg };
+export {
+  buyerImg,
+  investorImg,
+  defaultImg,
+  logoFinancial,
+  bgImg,
+  pic1,
+  pic2,
+  pic3,
+  pic4,
+  pic5,
+  iconCon,
+  iconDig,
+  iconConf,
+  iconSoc,
+  howImg1,
+  howImg2,
+  howImg3,
+};
