04c0bb38b3c90efd6ef54a378b6ea45edec24694
Angela Barrenechea <angelacba86@gmail.com>
2024-12-06 14:53:06 -0500

    create<component/header-login/headerLogin.jsx>create headerLogin

---
+++ client/src/components/header-login/headerLogin.jsx
@@ -0,0 +1,12 @@
+import { profileImg } from "../../assets/index";
+import { styles } from "./headerLogin.module.css";
+
+export function HeaderLogin() {
+  return (
+    <div className={styles.headerApp}>
+      <div className={styles.profile}>
+        <img src={profileImg} />
+      </div>
+    </div>
+  );
+}
