ca80c59652391f7b8cdb58f7a4c8502d648500ac
Angela Barrenechea <angelacba86@gmail.com>
2024-12-08 11:16:14 -0500

    create <components/financingBuyer/FinancingBuyer.jsx>create financingBuyer

---
+++ client/src/components/financing-buyer/financingBuyer.jsx
@@ -0,0 +1,9 @@
+export function FinancingBuyer() {
+  return (
+    <div>
+      <p style={{ color: "white", bakgroundColor: "blue" }}>
+        Este es el contenido de financiamiento
+      </p>
+    </div>
+  );
+}
