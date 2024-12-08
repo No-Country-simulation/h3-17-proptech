60f4ed015e84b125b65d0a58bf97ad40c07577bc
Angela Barrenechea <angelacba86@gmail.com>
2024-12-08 11:15:23 -0500

    modify <components/index.js>add components

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,15 +1,21 @@
 import { Footer as FooterComponent } from "./footer/footer";
+import { HeaderGeneral as HeaderGeneralComponent } from "./header-general/headerGeneral";
+import { HeaderAuth as HeaderAuthComponent } from "./header-auth/headerAuth";
 import { Header as HeaderComponent } from "./header/header";
 import { LoginForm as LoginFormComponent } from "./logIn-form/logIn";
 import { RegisterForm as RegisterFormComponent } from "./register-form/register";
 import { CardsWhy as CardWhyComponent } from "./cards-why/cardsWhy";
-import { HeaderLogin as HeaderLoginComponent } from "./header-login/headerLogin";
+import { SidebarBuyer } from "./sidebar-buyer/sidebarBuyer";
+import { FinancingBuyer as FinancingBuyerComponent } from "./financing-buyer/financingBuyer";
 
 export {
   FooterComponent,
+  HeaderGeneralComponent,
+  HeaderAuthComponent,
   HeaderComponent,
   LoginFormComponent,
   RegisterFormComponent,
   CardWhyComponent,
-  HeaderLoginComponent,
+  SidebarBuyer,
+  FinancingBuyerComponent,
 };
