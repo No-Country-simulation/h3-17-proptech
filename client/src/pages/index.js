c4c2c0f075698c7b761b1df97c7f61c22016fd9b
Angela Barrenechea <angelacba86@gmail.com>
2024-12-08 11:28:43 -0500

    modify<pages/index.js> add pages

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,5 +2,12 @@ import { LandingPage } from "./landing/landing";
 import { RegisterPage } from "./register/registerPage";
 import { LoginPage } from "./login/loginPage";
 import { RegisterOptions } from "./register-options/registerOption";
+import { DashboardLayout } from "./dashboard-layout/dashboardLayout";
 
-export { LandingPage, RegisterPage, LoginPage, RegisterOptions };
+export {
+  LandingPage,
+  RegisterPage,
+  LoginPage,
+  RegisterOptions,
+  DashboardLayout,
+};
