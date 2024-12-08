1985cf9c37ea6cced14a3aa5baf4dc4025ac6b50
MQuirico <128657405+MQuirico@users.noreply.github.com>
2024-12-08 15:52:18 -0300

    Logo redirecciona a home

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -40,11 +40,13 @@ export function Header() {
             }
           ></InputBase>
           <div className={styles.logo}>
+            <Link to="/">
             <img
-              style={{ width: "100%", height: "100%" }}
+              style={{ width: "100%", height: "100%", cursor: "pointer" }}
               src={logoFinancial}
               alt="logo Financial"
             />
+            </Link>
           </div>
           <div className={styles.buttonsContainer}>
             <Link to="/register-options">
