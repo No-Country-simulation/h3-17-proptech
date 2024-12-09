18f29fbbff3659ea490c1fb3c543447819ed9d90
MQuirico <128657405+MQuirico@users.noreply.github.com>
2024-12-09 17:14:56 -0300

    persistencia redux lograda

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,14 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from "react-router-dom"
-import {store} from './redux/store'
+import {store, persistor} from './redux/store'
 import { Provider } from 'react-redux'
+import { PersistGate } from "redux-persist/integration/react";
+
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <StrictMode>
       <BrowserRouter>
+      <PersistGate persistor={persistor}>
         <App />
+      </PersistGate>
       </BrowserRouter>
     </StrictMode>
   </Provider>
